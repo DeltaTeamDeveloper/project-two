8f60cb47737cc32327507eefc1fdad0f04f6411b
DeltaTeamDeveloper <hamza@dikings.us>
2023-09-18 08:10:23 -0700

    banner header footer

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 // import components
-import Homebanner from '../../components/Banner'
-import Contact from '../../components/Contact'
+import Homebanner from '../components/Banner'
+import Contact from '../components/Contact'
 
 
 export default function Home() {
